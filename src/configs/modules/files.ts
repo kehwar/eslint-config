1b1e91c7a777724eba9ac96ca539ad0fe0216adb
Kehwar <erickkwr@gmail.com>
2023-07-20 00:35:48 -0500

    fix: disable until bug fixed upstream

--- src/configs/modules/files.ts
+++ src/configs/modules/files.ts
@@ -4,7 +4,7 @@ export const getFilesConfig = createEslintConfig({
     /** Rules */
     rules: {
         /** @see https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/no-empty-file.md */
-        "unicorn/no-empty-file": "error",
+        "unicorn/no-empty-file": "off", // @see https://github.com/sindresorhus/eslint-plugin-unicorn/issues/2175
 
         /** @see https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/filename-case.md */
         "unicorn/filename-case": ["error", { case: "kebabCase" } ],
